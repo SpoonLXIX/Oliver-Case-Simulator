a961501633a562adad3e6012d92fed6144e46bfb
SpoonLXIX <spoonlxixdeveloper@gmail.com>
2025-06-02 17:57:07 +0100

    psuh

---
+++ oliver-case-simulator/main_game.gd
@@ -0,0 +1,13 @@
+extends Node2D
+##Define Buttons:
+@onready var cases = $"Menu Container/CanvasLayer/ColorRect/HBoxContainer/Cases"
+@onready var shop = $"Menu Container/CanvasLayer/ColorRect/HBoxContainer/Shop"
+@onready var olivers = $"Menu Container/CanvasLayer/ColorRect/HBoxContainer/Olivers"
+@onready var prestige = $"Menu Container/CanvasLayer/ColorRect/HBoxContainer/Prestige"
+@onready var rebirth = $"Menu Container/CanvasLayer/ColorRect/HBoxContainer/Rebirth"
+@onready var settings = $"Menu Container/CanvasLayer/ColorRect/VBoxContainer/Settings"
+@onready var leaderboard = $"Menu Container/CanvasLayer/ColorRect/VBoxContainer/Leaderboard"
+
+
+func _ready():
+	pass
