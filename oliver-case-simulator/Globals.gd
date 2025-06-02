e4226d6e4a6c431aa0303ba0a1275eada74c21a2
SpoonLXIX <spoonlxixdeveloper@gmail.com>
2025-06-02 18:21:56 +0100

    yippee

---
+++ oliver-case-simulator/Globals.gd
@@ -0,0 +1,5 @@
+extends Node
+var oliverBucks = 0
+var prestigePoints = 0
+var rebirthPoints = 0
+var bucksPerSecond = 1
--- oliver-case-simulator/main_game.gd
+++ oliver-case-simulator/main_game.gd
@@ -7,7 +7,28 @@ extends Node2D
 @onready var rebirth = $"Menu Container/CanvasLayer/ColorRect/HBoxContainer/Rebirth"
 @onready var settings = $"Menu Container/CanvasLayer/ColorRect/VBoxContainer/Settings"
 @onready var leaderboard = $"Menu Container/CanvasLayer/ColorRect/VBoxContainer/Leaderboard"
+@onready var second = $Second
+@onready var bucks = $"Menu Container/CanvasLayer/ColorRect/VBoxContainer2/OliverBucks"
+@onready var luck = $"Menu Container/CanvasLayer/ColorRect/VBoxContainer2/Luck"
+@onready var pp = $"Menu Container/CanvasLayer/ColorRect/VBoxContainer2/PrestigePoints"
+@onready var rp = $"Menu Container/CanvasLayer/ColorRect/VBoxContainer2/RebirthPoints"
 
 
 func _ready():
+	second.connect("timeout", _on_timer_timeout)
+
+func _process(_delta):
 	pass
+
+func secondPassed():
+	print("second")
+	Globals.oliverBucks = Globals.oliverBucks + Globals.bucksPerSecond
+	var buckstext = "Oliver Bucks: "
+	buckstext = buckstext + str(Globals.oliverBucks)
+	buckstext = buckstext + "   (" + str(Globals.bucksPerSecond) + "/s)"
+	bucks.text = buckstext
+	
+	
+	
+func _on_timer_timeout():
+	secondPassed()
